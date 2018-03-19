5b5c13ea9a305239a9b9cb12a18de0f0d834c426
Stan Koppe <36157870+stankoppe@users.noreply.github.com>
2018-03-19 16:45:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ function startTime() {
 
 
   if (h == 7){
+    TweenMax.to('#demo', 2.5, { ease: Sine.easeOut, y: -135 }); //tekst die omhoog gaat
     document.getElementById('background').style.backgroundImage= "url('landscape-2090495_1920.jpg')"; //sunrise
     document.getElementById("demo").innerHTML = "De zon komt nu op";
   }
@@ -31,10 +32,12 @@ function startTime() {
 
   }
   else if (h == 19){
+    TweenMax.to('#demo', 2.5, { ease: Sine.easeOut, y: 150 });  // tekst die naar beneden gaat
     document.getElementById('background').style.backgroundImage= "url('sunset-3220635_1920.jpg')"; //sunset
     document.getElementById("demo").innerHTML = "De zon gaat nu onder";
   }
   else {
+    TweenMax.to('#demo', 5,{opacity: 0})
     document.getElementById('background').style.backgroundImage= "url('red-planet-571902_1920.jpg')"; //al het overige
     document.getElementById("demo").innerHTML = "Succes vandaag!";
   }
